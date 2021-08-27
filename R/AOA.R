ac39bbb9c37fa4b9c8b956b061f4b0c8077d43b2
mlezamavaldes <mlezamavaldes@gmail.com>
2021-08-27 13:52:15 +0200

    data_documentation_test

---
+++ R/AOA.R
@@ -0,0 +1,16 @@
+#' Area of Applicability for the example scene 
+#'
+#' Area of Applicability for the example scene. 
+#'
+#' @docType data
+#'
+#' @usage data(AOA)
+#'
+#' @format An object of class \code{"raster"}.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(AOA)
+#' plot(AOA)
+"AOA"
